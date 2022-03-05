3668b412be1e65f83b24cace3be1527b7be34abc
Soodit Kumar <50576944+SooditK@users.noreply.github.com>
2022-03-05 23:27:48 +0530

    feat(sooditk): User SignIn, Lab Signup/SignIn done
    #7

---
+++ components/ContactUs.tsx
@@ -0,0 +1,83 @@
+import { Textarea } from '@mantine/core'
+import React from 'react'
+
+const ContactUs = () => {
+  return (
+    <>
+      <form className="mt-8 space-y-6" action="#" method="POST">
+        <input type="hidden" name="remember" defaultValue="true" />
+        <div className="-space-y-px rounded-md shadow-sm">
+          <div>
+            <label htmlFor="name" className="sr-only">
+              Your Name
+            </label>
+            <input
+              id="name"
+              name="name"
+              type="name"
+              autoComplete="name"
+              required
+              className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              placeholder="Your Name"
+            />
+          </div>
+          <div>
+            <label htmlFor="email" className="sr-only">
+              Your Email
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              required
+              className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              placeholder="Your Email"
+            />
+          </div>
+          <label htmlFor="message" className="sr-only">
+            Your Message
+          </label>
+          <Textarea placeholder="Your Message" radius="md" required />
+        </div>
+
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <input
+              id="remember-me"
+              name="remember-me"
+              type="checkbox"
+              className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+            />
+            <label
+              htmlFor="remember-me"
+              className="ml-2 block text-sm text-gray-900"
+            >
+              Remember me
+            </label>
+          </div>
+
+          <div className="text-sm">
+            <a
+              href="#"
+              className="font-medium text-indigo-600 hover:text-indigo-500"
+            >
+              Forgot your password?
+            </a>
+          </div>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            Sign in
+          </button>
+        </div>
+      </form>
+    </>
+  )
+}
+
+export default ContactUs
