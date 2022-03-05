e62c66f425919d466f0449e695e424a6777f06f2
officialpiyush <officialpiyush@users.noreply.github.com>
2022-03-05 17:17:04 +0000

    Prettified Code!

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,20 +6,20 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default NextAuth({
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        }),
-    ],
-    session: {
-        jwt: true,
-        maxAge: 30 * 24 * 60 * 60,
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-    jwt: {
-        signingKey: process.env.JWT_SIGNING_PRIVATE_KEY,
-    },
-    database: process.env.DATABASE_URL,
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  session: {
+    jwt: true,
+    maxAge: 30 * 24 * 60 * 60,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  jwt: {
+    signingKey: process.env.JWT_SIGNING_PRIVATE_KEY,
+  },
+  database: process.env.DATABASE_URL,
 })
