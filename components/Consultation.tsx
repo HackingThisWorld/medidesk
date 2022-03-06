3d6e3e889d63a0a2c63fc5690ff6d28eb13c5247
Samyak Jain <samyakjainking@gmail.com>
2022-03-06 14:29:58 +0530

    added Consultation form

--- components/Consultation.tsx
+++ components/Consultation.tsx
@@ -2,7 +2,8 @@ import { Group, MantineTheme, Text, useMantineTheme } from '@mantine/core'
 import { ImageIcon, UploadIcon, CrossCircledIcon } from '@modulz/radix-icons'
 import { Dropzone, IMAGE_MIME_TYPE } from '@mantine/dropzone'
 import { DatePicker } from '@mantine/dates'
-import { useState,useEffect } from 'react'
+import { Select } from '@mantine/core'
+import { useState, useEffect } from 'react'
 function ImageUploadIcon({ status, ...props }) {
   if (status.accepted) {
     return <UploadIcon {...props} />
@@ -34,101 +35,22 @@ const BookModal = () => {
   const [lastName, setLastName]: any = useState()
   const [email, setEmail]: any = useState()
   const [hospital, setHospital]: any = useState()
+  const [condition, setCondition]: any = useState()
   const [prescription, setPrescription]: any = useState()
   useEffect(() => {
-    
-  console.log(date)
-    
+    console.log(date)
   }, [date])
-  
+
   const theme = useMantineTheme()
   return (
     <>
       <form
-        className="mt-8 space-y-6"
+        className="mt-8 space-y-6 overflow-y-auto"
         onSubmit={(e) => e.preventDefault()}
         action="https://eouwzr6y8ixcj5i.m.pipedream.net/"
         encType="multipart/form-data"
         method="POST"
       >
-        <div className="-space-y-px rounded-md shadow-sm">
-          <div className="my-4">
-            <label htmlFor="first_name" className="">
-              First Name
-            </label>
-            <input
-              id="name"
-              name="first_name"
-              type="name"
-              required
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-              className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm"
-              placeholder="Your First Name"
-            />
-          </div>
-        </div>
-        <div className="-space-y-px rounded-md shadow-sm">
-          <div className="my-4">
-            <label htmlFor="last_name" className="">
-              Last Name
-            </label>
-            <input
-              id="name"
-              name="last_name"
-              type="name"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-              required
-              className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm"
-              placeholder="Your Last Name"
-            />
-          </div>
-        </div>
-        <div className="-space-y-px rounded-md shadow-sm">
-          <div className="my-4">
-            <label htmlFor="hospital_name" className="">
-              Prescribed By :
-            </label>
-            <input
-              id="name"
-              name="hospital_name"
-              type="name"
-              required
-              value={hospital}
-              onChange={(e) => setHospital(e.target.value)}
-              className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm"
-              placeholder="Your Hospital"
-            />
-          </div>
-        </div>
-        <div className="my-4">
-          <label htmlFor="email" className="">
-            Your Email :
-          </label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            autoComplete="email"
-            required
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm"
-            placeholder="Your Email"
-          />
-        </div>
-
-        <div className="-space-y-px rounded-md shadow-sm">
-          <div className="my-4">
-            <label htmlFor="hospital_name" className="">
-              Prescription Date :
-            </label>
-
-            <DatePicker value={date} onChange={()=> setDate} />
-          </div>
-        </div>
-      
         <div className="-space-y-px rounded-md shadow-sm">
           <div className="my-4">
             <label htmlFor="prescription_file" className="">
@@ -170,12 +92,105 @@ const BookModal = () => {
             </Dropzone>
           </div>
         </div>
+        <div className="-space-y-px rounded-md shadow-sm">
+          <div className="my-4">
+            <label htmlFor="first_name" className="">
+              First Name
+            </label>
+            <input
+              id="name"
+              name="first_name"
+              type="name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              required
+              className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm"
+              placeholder="Your First Name"
+            />
+          </div>
+        </div>
+        <div className="-space-y-px rounded-md shadow-sm">
+          <div className="my-4">
+            <label htmlFor="last_name" className="">
+              Last Name
+            </label>
+            <input
+              id="name"
+              name="last_name"
+              type="name"
+              required
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm"
+              placeholder="Your Last Name"
+            />
+          </div>
+        </div>
+        <div className="-space-y-px rounded-md shadow-sm">
+          <div className="my-4">
+            <label htmlFor="hospital_name" className="">
+              Prescribed By :
+            </label>
+            <input
+              id="name"
+              name="hospital_name"
+              type="name"
+              required
+              value={hospital}
+              onChange={(e) => setHospital(e.target.value)}
+              className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm"
+              placeholder="Your Hospital"
+            />
+          </div>
+        </div>
+        <div className="my-4">
+          <label htmlFor="email" className="">
+            Your Email :
+          </label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            autoComplete="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm"
+            placeholder="Your Email"
+          />
+        </div>
+
+        <div className="-space-y-px rounded-md shadow-sm">
+          <div className="my-4">
+            <label htmlFor="Prescription Date" className="">
+              Consultation Date :
+            </label>
+            <DatePicker
+              value={date}
+              placeholder="Pick Consultation Date"
+              onChange={setDate}
+              name={'Prescription Date'}
+              defaultValue={new Date()}
+            />
+          </div>
+        </div>
+        <Select
+          value={condition}
+          onChange={setCondition}
+          label="health condition"
+          placeholder="Pick one"
+          data={[
+            { value: 'low', label: 'Low' },
+            { value: 'mild', label: 'Mild' },
+            { value: 'severe', label: 'Severe' },
+          ]}
+        />
         <div>
           <button
             type="submit"
             className="group relative flex w-full justify-center rounded-md border border-transparent bg-green-600 py-2 px-4 text-sm font-medium text-white hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
           >
-            Book Service
+            Book Consultation
           </button>
         </div>
       </form>
