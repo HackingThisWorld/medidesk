34c8bff87af841b1d3ace3390fb078992004d76a
Piyush Bhangale <bhangalepiyush@gmail.com>
2022-03-06 16:52:34 +0530

    form: changes

--- pages/services.tsx
+++ pages/services.tsx
@@ -14,7 +14,7 @@ export default function Services() {
             className="flex flex-col justify-start pt-2 pb-4"
           >
             <Tabs.Tab label="Services">
-              <div className="grid grid-cols-1 gap-8 p-5 md:grid-cols-2 lg:grid-cols-3">
+              <div className="place-items-center grid grid-cols-1 gap-8 p-5 md:grid-cols-2 lg:grid-cols-3">
                 {new Array(5).fill(0).map((_, index) => (
                   <ServiceCard key={index} />
                 ))}
